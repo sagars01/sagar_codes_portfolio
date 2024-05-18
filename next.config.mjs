dc38be16fa56996d5c294cb500f832debe716792
Sagar <sagarmoysengupta@gmail.com>
2024-05-18 12:01:22 +0530

    Some og images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        metadataBase: 'https://www.withjessi.com'
+    },
+};
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,33 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const imgUrl = "/imgs/og.png"
+
 export const metadata = {
   title: "Welcome to Sagarmoy Sengupta Web",
   description: "Sagar Codes Digital Placeholder",
+  metadataBase: new URL("https://www.withjessi.com"),
+  openGraph: {
+    title: 'Sagarmoy Sengupta',
+    description: 'The calmest person in tech',
+    url: 'https://www.sagarmoy.work',
+    siteName: 'Sagar Codes Portfolio',
+    images: [
+      {
+        url: imgUrl,
+        width: 800,
+        height: 600,
+      },
+      {
+        url: imgUrl,
+        width: 1800,
+        height: 1600,
+        alt: 'Sagar Codes Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
 };
 
 export default function RootLayout({ children }) {
