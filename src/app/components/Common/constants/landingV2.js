99272190bd3cc12c796464557865d74d3b6e6584
Sagar <sagarmoysengupta@gmail.com>
2024-05-24 13:28:14 +0530

    to update the content

--- src/app/components/Common/constants/landingV2.js
+++ src/app/components/Common/constants/landingV2.js
@@ -2,12 +2,12 @@ export default {
     "heroText": {
         "name": "Sagar",
         "greeting": "Hi, I'm Sagar 👋",
-        "description": "I'm a passionate software engineer with an interest in",
+        "description": "I'm a staff software engineer with an interest in",
         "rotatingTexts": [
             "Software Architecture",
-            "Backend Architecture",
-            "Frontend Engineering",
-            "Database Optimisation"
+            "Scaling Backend to Millions",
+            "UX and Frontend Engineering",
+            "Database Optimisation for Billion Records"
         ],
         "buttons": [
             {
@@ -22,27 +22,19 @@ export default {
         "contactIcons": [
             {
                 "type": "email",
-                "href": "mailto:example@example.com"
-            },
-            {
-                "type": "phone",
-                "href": "tel:+123456789"
+                "href": "sagarmoy.goww@outlook.com"
             },
             {
                 "type": "linkedin",
-                "href": "https://linkedin.com/in/johndoe"
+                "href": "https://linkedin.com/in/sagars01"
             },
             {
                 "type": "github",
-                "href": "https://github.com/johndoe"
+                "href": "https://github.com/sagars01"
             },
             {
                 "type": "twitter",
-                "href": "https://twitter.com/johndoe"
-            },
-            {
-                "type": "youtube",
-                "href": "https://youtube.com/johndoe"
+                "href": "https://twitter.com/sagar_codes"
             }
         ]
     }
