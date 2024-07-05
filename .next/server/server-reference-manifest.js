7c148e7c1d4c9a349172e4860163e9f9df72097b
Sagar <sagarmoysengupta@gmail.com>
2024-07-05 12:55:28 +0530

    Clean up 2

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IXstDQ4DX8Y2XC4+AFxpcx98oCs57fdIYPmzmHqJ4AU=\"\n}"
--- components/preview/CopyRight.jsx
+++ components/preview/CopyRight.jsx
@@ -6,14 +6,7 @@ export default function CopyRight() {
       <div className="text-center">
         <span>
           © {new Date().getFullYear()} All Rights Reserved by{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://themeforest.net/user/elite-themes24"
-          >
-            Elite-Themes24
-          </a>
-          .
+          Sagarmoy Sengupta
         </span>
       </div>
     </div>
